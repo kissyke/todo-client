b27e8ed97958d39e2a16209b7f36dcb37a0c7108
kissyke <kissyke@gmail.com>
2020-12-14 15:52:15 +0100

    UseCallback changes in context

--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useCallback } from "react";
 
 const TodoContext = React.createContext();
 
@@ -14,14 +14,14 @@ const TodoProvider = ({ children }) => {
     type: "",
   });
 
-  const showAlert = (show = false, type = "", msg = "") => {
+  const showAlert = useCallback((show = false, type = "", msg = "") => {
     setAlert({ show, type, msg });
-  };
+  }, []);
 
   const sortTodoItems = (a, b) =>
     (a.date || Number.POSITIVE_INFINITY) - (b.date || Number.POSITIVE_INFINITY);
 
-  const fetchData = async () => {
+  const fetchData = useCallback(async () => {
     const result = await fetch("/api/todos");
     const list = await result.json();
     for (const item of list) {
@@ -31,112 +31,143 @@ const TodoProvider = ({ children }) => {
     }
     list.sort(sortTodoItems);
     setTodoItems(list);
-  };
+  }, []);
 
-  const addTodo = async (text, date) => {
-    const newTodo = { text, date };
-    newTodo.date = newTodo.date ? newTodo.date.toString() : newTodo.date;
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newTodo),
-    };
-    try {
-      const result = await fetch("/api/todos", requestOptions);
-      const newItem = await result.json();
-      const fixedNewItem = {
-        ...newItem,
-        date: newItem.date ? new Date(newItem.date) : newItem.date,
+  const addTodo = useCallback(
+    async (text, date) => {
+      const newTodo = { text, date };
+      newTodo.date = newTodo.date ? newTodo.date.toString() : newTodo.date;
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newTodo),
       };
-      const newTodos = [...todoItems, fixedNewItem];
+      try {
+        const result = await fetch("/api/todos", requestOptions);
+        const newItem = await result.json();
+        const fixedNewItem = {
+          ...newItem,
+          date: newItem.date ? new Date(newItem.date) : newItem.date,
+        };
+        const newTodos = [...todoItems, fixedNewItem];
+        newTodos.sort(sortTodoItems);
+        setTodoItems(newTodos);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [todoItems]
+  );
+
+  const updateTodo = useCallback(
+    async (todo) => {
+      const fixedTodo = {
+        ...todo,
+        date: todo.date ? todo.date.toString() : todo.date,
+      };
+      const requestOptions = {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(fixedTodo),
+      };
+      const result = await fetch(`/api/todos/${todo._id}`, requestOptions);
+      const updatedItem = await result.json();
+      const fixedUpdatedItem = {
+        ...updatedItem,
+        date: updatedItem.date ? new Date(updatedItem.date) : updatedItem.date,
+      };
+      const newTodos = todoItems.map((item) =>
+        item._id === fixedUpdatedItem._id ? { ...fixedUpdatedItem } : item
+      );
       newTodos.sort(sortTodoItems);
       setTodoItems(newTodos);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    },
+    [todoItems]
+  );
 
-  const updateTodo = async (todo) => {
-    const fixedTodo = {
-      ...todo,
-      date: todo.date ? todo.date.toString() : todo.date,
-    };
-    const requestOptions = {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(fixedTodo),
-    };
-    const result = await fetch(`/api/todos/${todo._id}`, requestOptions);
-    const updatedItem = await result.json();
-    const fixedUpdatedItem = {
-      ...updatedItem,
-      date: updatedItem.date ? new Date(updatedItem.date) : updatedItem.date,
-    };
-    const newTodos = todoItems.map((item) =>
-      item._id === fixedUpdatedItem._id ? { ...fixedUpdatedItem } : item
-    );
-    newTodos.sort(sortTodoItems);
-    setTodoItems(newTodos);
-  };
+  const toggleCompleteTodo = useCallback(
+    async (id) => {
+      const specificItem = todoItems.find((item) => item._id === id);
+      specificItem.isCompleted = !specificItem.isCompleted;
+      updateTodo(specificItem);
+    },
+    [todoItems, updateTodo]
+  );
 
-  const toggleCompleteTodo = async (id) => {
-    const specificItem = todoItems.find((item) => item._id === id);
-    specificItem.isCompleted = !specificItem.isCompleted;
-    updateTodo(specificItem);
-  };
+  const removeTodo = useCallback(
+    async (id) => {
+      const requestOptions = {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      };
+      try {
+        await fetch(`/api/todos/${id}`, requestOptions);
+        const newTodos = todoItems.filter((item) => item._id !== id);
+        showAlert(true, "danger", "item removed");
+        setTodoItems(newTodos);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [todoItems, showAlert]
+  );
 
-  const removeTodo = async (id) => {
-    const requestOptions = {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
-    };
-    try {
-      await fetch(`/api/todos/${id}`, requestOptions);
-      const newTodos = todoItems.filter((item) => item._id !== id);
-      showAlert(true, "danger", "item removed");
-      setTodoItems(newTodos);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const editItem = useCallback(
+    (id) => {
+      const specificItem = todoItems.find((item) => item._id === id);
+      setIsEditing(true);
+      setEditID(id);
+      setName(specificItem.text);
+      setSelectedDate(
+        specificItem.date ? new Date(specificItem.date) : new Date()
+      );
+    },
+    [todoItems]
+  );
 
-  const editItem = (id) => {
-    const specificItem = todoItems.find((item) => item._id === id);
-    setIsEditing(true);
-    setEditID(id);
-    setName(specificItem.text);
-    setSelectedDate(
-      specificItem.date ? new Date(specificItem.date) : new Date()
-    );
-  };
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!name) {
+        showAlert(true, "danger", "please enter value");
+      } else if (name && isEditing) {
+        const specificItem = todoItems.find((item) => item._id === editID);
+        const modifiedItem = {
+          ...specificItem,
+          text: name,
+          date: selectedDate,
+        };
+        updateTodo(modifiedItem);
+        setName("");
+        setSelectedDate(null);
+        setEditID(null);
+        setIsEditing(false);
+        showAlert(true, "success", "value changed");
+      } else {
+        showAlert(true, "success", "item added to the list");
+        addTodo(name, selectedDate);
+        setName("");
+        setSelectedDate(null);
+      }
+    },
+    [
+      todoItems,
+      editID,
+      name,
+      isEditing,
+      selectedDate,
+      showAlert,
+      addTodo,
+      updateTodo,
+    ]
+  );
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!name) {
-      showAlert(true, "danger", "please enter value");
-    } else if (name && isEditing) {
-      const specificItem = todoItems.find((item) => item._id === editID);
-      const modifiedItem = { ...specificItem, text: name, date: selectedDate };
-      updateTodo(modifiedItem);
-      setName("");
-      setSelectedDate(null);
-      setEditID(null);
-      setIsEditing(false);
-      showAlert(true, "success", "value changed");
-    } else {
-      showAlert(true, "success", "item added to the list");
-      addTodo(name, selectedDate);
-      setName("");
-      setSelectedDate(null);
-    }
-  };
-
-  const handleReset = () => {
+  const handleReset = useCallback(() => {
     setEditID(null);
     setIsEditing(false);
     setName("");
     setSelectedDate(null);
-  };
+  }, []);
 
   return (
     <TodoContext.Provider
@@ -145,7 +176,6 @@ const TodoProvider = ({ children }) => {
         showAlert,
         todoItems,
         fetchData,
-        sortTodoItems,
         addTodo,
         updateTodo,
         toggleCompleteTodo,
